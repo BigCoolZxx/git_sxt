f2a66c97d7741c4d67da9d8907ed4c0bae179923
BigCoolZxx <46020361+BigCoolZxx@users.noreply.github.com>
2018-12-20 22:04:18 +0800

    Update MyGit.java

--- TestGit/src/cn/gzsxt/test/MyGit.java
+++ TestGit/src/cn/gzsxt/test/MyGit.java
@@ -4,7 +4,7 @@ public class MyGit {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("this is the second ties");
-		System.out.println("add in the browse");
+		
 	}
 	
 	public void testMerge() {
